26afd6bbf9367780be44d3b19ef911a58eeab068
Josseycodes <adewumijosephine1@gmail.com>
2026-02-27 20:25:47 +0100

    another bug

--- src/app/(main)/checkout/CheckoutClient.tsx
+++ src/app/(main)/checkout/CheckoutClient.tsx
@@ -26,7 +26,7 @@ export default function CheckoutPage() {
 
     const fetchProduce = async () => {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/produce/${produceId}`,
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/produce/${produceId}`,
       );
 
       const data = await res.json();
